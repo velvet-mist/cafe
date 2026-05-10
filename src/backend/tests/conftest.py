d3f94da0d6f699d758566da708f15ac6d7ccfda8
Sneha <sssnehasinha787@gmail.com>
2026-05-10 13:54:17 +0530

    making the backend work

--- src/backend/tests/conftest.py
+++ src/backend/tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.main import app
+from cafe.src.backend.main import app
 from app.database import Base
 from app.dependencies import get_db
 
